db61e66d14e051d26151f46f058b98f74b649fb7
zzxcncz <zhuang1994@foxmail.com>
2024-05-10 08:54:35 +0800

    zzx modify windows push @ 2024-05-10_08-54-35

--- .history/GitPush_20240426085948.sh
+++ .history/GitPush_20240426085948.sh
@@ -5,6 +5,6 @@ time=`date "+%Y-%m-%d_%H-%M-%S"`
 who="zzx"
 git add .
 git commit -m "${who} $commit push @ ${time}"
-git push -u origin master
-#git push -u github master
+#git push -u origin master
+git push -u github master
 echo "Finished Push"
